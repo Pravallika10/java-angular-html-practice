6c52c4c651f917caf2ebc86f8396b3910e810991
Pravallika10 <60029173+Pravallika10@users.noreply.github.com>
2020-02-07 10:03:48 +0530

    Add files via upload

--- java/springwebmvc/src/com/cts/trainee/springwebmvc/StudentController.java
+++ java/springwebmvc/src/com/cts/trainee/springwebmvc/StudentController.java
@@ -1,10 +1,18 @@
 package com.cts.trainee.springwebmvc;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import com.cts.trainee.spring.web.model.Student;
+
+/*   @Controller
 @RequestMapping("/student")
 public class StudentController {
+
 	@RequestMapping("/dashboard")
 	public String home() {
 		return "student-home";
@@ -12,8 +20,52 @@ public class StudentController {
 	
 	@RequestMapping("/profile")
 	public String profile() {
+		Student student = new Student();
+		student.setUname("Dummy");
 		return "student-profile";
 	}
+	@RequestMapping("/save") // fallback method for bad url
+	public String saveData(Student student,Model model) {
+		System.out.println("\nName : " + student.getUname());
+		System.out.println("Email : " + student.getEmail());
+		student.setUname(student.getUname().toUpperCase());
+		student.setEmail(student.getEmail().toUpperCase());
+		model.addAttribute("stud",student);
+		return "student-confirm";
+	} */
+	
+	
+	@Controller
+	@RequestMapping("/student")
+	public class StudentController {
+
+		@RequestMapping("/dashboard")
+		public String home() {
+			return "student-home";
+		}
+		
+		@RequestMapping("/profile")
+		public String profile(Model model) 
+		{
+			Student student = new Student();
+			//student.setUname("Dummy");
+			model.addAttribute("student", student);
+			return "student-profile";
+		}
+		
+		@RequestMapping("/save")
+		public String save(@ModelAttribute Student student) 
+		{
+			System.out.println("NAME : " + student.getUname());
+			System.out.println("EMAIL : " + student.getEmail());
+			return "student-confirm";
+		}
+		
+		
+	
+	
+	
+	
 	
 	// mapping action method with parent url
 	@RequestMapping() // default method for parent url
@@ -26,5 +78,6 @@ public class StudentController {
 	public String fallbackMethod() {
 		return "student-error";
 	}
-
+	
+	
 }
---
+++ java/springwebmvc/src/com/cts/trainee/springwebmvc/Testcontroller.java
@@ -0,0 +1,7 @@
+package com.cts.trainee.springwebmvc;
+import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class Testcontroller {
+
+}
